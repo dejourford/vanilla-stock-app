d9bc339e1066bcd9cbff7f0c37b89ee30979e20b
dejourford <dejourford@my.unt.edu>
2023-11-12 00:06:52 -0600

    added get balance function to objects

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 // create new array for users
+let users = []
+let user
 // create object for new user
-// capture input value and store as variable
+function User (id,username,password, balance) {
+    this.id = id,
+    this.username = username,
+    this.password = password,
+    this.balance = balance,
+    this.getBalance = function getBalance() {
+        console.log(`${this.username} has a balance of ${this.balance}`)
+    }
+}
+// capture input value and store as variable on button press
+const logInButton = document.querySelector('.log-in-button')
+logInButton.addEventListener('click', createNewUser)
 // create new user using captured values on button press
+function createNewUser() {
+    console.log('a new user has been created')
+    let usernameInputValue = document.querySelector('.username-input-value').value
+    let passwordInputValue = document.querySelector('.password-input-value').value
+    let balance = 0
 // push user object to users array
+    user = new User(users.length + 1, usernameInputValue, passwordInputValue, balance)
+    users.push(user)
+    console.log(users)
+}
+
+// display updated array
+function updateArray(users) {
+    console.log(users)
+    users = users
+}
+
