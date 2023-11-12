afe5a9417d1757767b8250f91fe52d338e9d67cf
dejourford <dejourford@my.unt.edu>
2023-11-11 23:28:26 -0600

    add pseudocode for login feature

---
+++ index.js
@@ -0,0 +1,5 @@
+// create new array for users
+// create object for new user
+// capture input value and store as variable
+// create new user using captured values on button press
+// push user object to users array
