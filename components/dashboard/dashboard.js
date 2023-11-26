46036ed3c8e1063aa98a4e8531ea1d7ddd815b64
dejourford <dejourford@my.unt.edu>
2023-11-25 22:15:14 -0600

    create function for uppercase name

---
+++ components/dashboard/dashboard.js
@@ -0,0 +1,72 @@
+
+
+const ls = localStorage
+const currentUser = JSON.parse(ls.getItem('currentUser'))
+console.log(`Welcome ${currentUser.username} `)
+
+const playButton =  document.querySelector('.play-button')
+const pauseButton =  document.querySelector('.pause-button')
+const backgroundMusic = document.querySelector('#backgroundMusic')
+const buttonPressSound = document.querySelector('#button-press-sound')
+const header = document.querySelector('header')
+
+// on windows load, set play button to hidden
+playButton.style.display = 'none'
+
+// on windows load, set the music volume
+backgroundMusic.volume = 0.08
+
+// button press audio sounds
+
+
+// create a play sound function
+function playSound(e) {
+    console.log(e.target)
+    if (e.target === logInButton) {
+        buttonPressSound.play()
+    }
+}
+
+// if user clicks button, peform that function
+playButton.addEventListener('click', function() {
+    backgroundMusic.play()
+    playButton.style.display = 'none'
+    pauseButton.style.display = 'flex'
+})
+
+pauseButton.addEventListener('click', function() {
+    backgroundMusic.pause()
+    pauseButton.style.display = 'none'
+    playButton.style.display = 'flex'
+    console.log('the music has been paused')
+})
+
+// MAIN DASHBOARD JS CONTENT
+
+
+// TASK: CAPITALIZE FIRST LETTER OF USERNAME
+
+
+
+
+function capitalizeUsername(name) {
+    // receive username
+    console.log(name)
+    // grab first character of username
+    // capitalize character
+    const letter = name.at(0).toUpperCase()
+    console.log(letter)
+    // add character back to name
+    const updatedName = letter + name.slice(1)
+    console.log(updatedName)
+    // return result
+    return updatedName
+}
+
+// display welcome message
+const h1 = document.querySelector('h1')
+h1.innerHTML = `Welcome, <br> ${capitalizeUsername(currentUser.username)}!`
+
+// display balance amount
+const balanceAmount = document.querySelector('.balance-amount')
+balanceAmount.innerHTML = `$${currentUser.balance}`
