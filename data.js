388a709db787da4cd17a2e80499e12dabebb368a
dejourford <dejourford@my.unt.edu>
2023-12-02 07:23:17 -0600

    create data file of stock list

---
+++ data.js
@@ -0,0 +1,98 @@
+const stockList = [
+    {
+        id: 1,
+        title: 'aapl',
+    
+    },
+    {
+        id: 2,
+        title: 'msft',
+    },
+    {
+        id: 3,
+        title: 'amzn',
+    },
+    {
+        id: 4,
+        title: 'nvda',
+    },
+    {
+        id: 5,
+        title: 'meta',
+    },
+    {
+        id: 6,
+        title: 'goog',
+    },
+    {
+        id: 7,
+        title: 'tsla',
+    },
+    {
+        id: 8,
+        title: 'adbe',
+    },{
+        id: 9,
+        title: 'pep',
+    },
+    {
+        id: 10,
+        title: 'nflx',
+    },
+    {
+        id: 11,
+        title: 'tmus',
+    },
+    {
+        id: 12,
+        title: 'sbux',
+    },
+    {
+        id: 13,
+        title: 'pypl',
+    },
+    {
+        id: 14,
+        title: 'mar',
+    },
+    {
+        id: 15,
+        title: 'lulu',
+    },
+    {
+        id: 16,
+        title: 'abnb',
+    },
+    {
+        id: 17,
+        title: 'rost',
+    },
+    {
+        id: 18,
+        title: 'ea',
+    },
+    {
+        id: 19,
+        title: 'mrna',
+    },
+    {
+        id: 20,
+        title: 'ebay',
+    },
+    {
+        id: 21,
+        title: 'zm',
+    },
+    {
+        id: 22,
+        title: 'wba',
+    },
+    {
+        id: 23,
+        title: 'dltr',
+    },
+    {
+        id: 24,
+        title: 'cost',
+    },
+]
