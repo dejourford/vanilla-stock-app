d5919923b59dc1b2d07ce229f00a649141063245
dejourford <dejourford@my.unt.edu>
2023-12-16 16:33:51 -0600

    add balance to html element

--- components/BuyPage/buypage.js
+++ components/BuyPage/buypage.js
@@ -1,6 +1,12 @@
+const ls = localStorage
 const homeButton = document.querySelector('#home-button')
-
+const message = document.querySelector('.message')
+const currentUser = JSON.parse(ls.getItem('currentUser'))
+const balance = document.querySelector('.balance-amount')
 // TASK: CREATE FUNCTION FOR BUTTON REDIRECTS
 homeButton.addEventListener('click', () => {
     window.location = '../dashboard/dashboard.html'
 })
+
+// TASK: DISPLAY BALANCE FOR CURRENT USER
+balance.innerHTML = `$${currentUser.balance}`
