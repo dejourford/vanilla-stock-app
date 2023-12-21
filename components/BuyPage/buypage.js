00aedef70b1356b994aef7a3a8dbdbf65509d352
dejourford <dejourford@my.unt.edu>
2023-12-21 15:52:40 -0600

    add loading spinner

--- components/BuyPage/buypage.js
+++ components/BuyPage/buypage.js
@@ -4,9 +4,32 @@ const message = document.querySelector('.message')
 const currentUser = JSON.parse(ls.getItem('currentUser'))
 const balance = document.querySelector('.balance-amount')
 const buyButton = document.querySelector('#buy-button')
+const logoutButton = document.querySelector('#logout-button')
+const logoutSound = document.querySelector('#logout-sound')
+const loadingContainer = document.querySelector('.loading-container')
+const main = document.querySelector('main')
 // TASK: CREATE FUNCTION FOR BUTTON REDIRECTS
 homeButton.addEventListener('click', () => {
     window.location = '../dashboard/dashboard.html'
+})
+
+// button press audio sounds
+logoutSound.volume = 0.3
+
+// on windows load, set loading properties
+loadingContainer.style.display = 'none';
+main.style.display = 'flex';
+
+// create event listener for logout button
+logoutButton.addEventListener('click', () => {
+  logoutSound.play()
+  loadingContainer.style.display = 'flex';
+    main.style.display = 'flex';
+  setTimeout(() => {
+    
+    window.location = '../../index.html'
+  }, 500)
+  
 })
 
 // TASK: DISPLAY BALANCE FOR CURRENT USER
