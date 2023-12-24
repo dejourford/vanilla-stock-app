dae9b7635bc874f013d321df03fb2c42fe029209
dejourford <dejourford@my.unt.edu>
2023-12-23 23:49:35 -0600

    create dom manipulation functionlity for deposits

--- components/CreditCardPaymentPage/creditcardpaymentpage.js
+++ components/CreditCardPaymentPage/creditcardpaymentpage.js
@@ -1,20 +1,23 @@
-
-
+// define button elements
+const buttonAmounts = document.querySelectorAll('.button-amount')
+const depositAmountField = document.querySelector('#deposit-amount')
 const backArrow = document.querySelector('.back-arrow-button')
+const transactionText = document.querySelector('.transaction-text')
+
 backArrow.addEventListener('click', () => {
     window.location = '../AddFundsPage/addfundspage.html'
 })
 
-// TASK: CREATE FUNCTION FOR FOCUSED BUTTONS
 
-// define button elements
-const buttonAmounts = document.querySelectorAll('.button-amount')
+
+// TASK: CREATE FUNCTION FOR FOCUSED BUTTONS
 // create selected button variable
 let selectedButton
 // create function to remove border from prev button
 function removeBorderFromSelected() {
     if(selectedButton) {
         selectedButton.classList.remove('selected')
+        transactionText.textContent = ''
     }
 }
 // add click event listeners to buttons
@@ -27,6 +30,27 @@ buttonAmounts.forEach(button => {
         // set border to newly clicked button
         currentButton.classList.add('selected')
         selectedButton = currentButton
+
+        // TASK: CREATE FUNCTION FOR DISPLAYING A SELECTED AMOUNT
+        // if an amount is selected, display it in the DOM
+        const currentValue =  currentButton.getAttribute('data-amount')
+        depositAmountField.value = '$' + currentValue
+
+        // generate transaction message for button amount
+        transactionText.textContent = `You will be charged $${currentValue} by Stock`
     })
    
+})
+
+// TASK: REMOVE BORDER FROM BUTTONS AND CLEAR INPUT FIELD
+// IF IT IS CLICKED
+depositAmountField.addEventListener('click', () => {
+    removeBorderFromSelected()
+    depositAmountField.value = '$'
+})
+
+// TASK: DISPLAY TRANSACTION MESSAGE IF INPUT FIELD HAS VALUE
+depositAmountField.addEventListener('change', () => {
+    const currentValue = depositAmountField.value
+    transactionText.textContent = `You will be charged ${currentValue} by Stock`
 })
