9facb1149ccf44cbd345c5802b0802a1896910fc
dejourford <dejourford@my.unt.edu>
2024-02-03 17:49:22 -0600

    copy working code to other pages

--- components/ApplepayPaymentPage/applepaypaymentpage.js
+++ components/ApplepayPaymentPage/applepaypaymentpage.js
@@ -4,12 +4,19 @@ const depositAmountField = document.querySelector('#deposit-amount')
 const backArrow = document.querySelector('.back-arrow-button')
 const transactionText = document.querySelector('.transaction-text')
 const depositButton = document.querySelector('.deposit-button')
+const form = document.querySelector('form')
+const balance = document.querySelector('.balance')
+const ls = localStorage
+const user = JSON.parse(ls.getItem('currentUser'))
+const purchaseSound = document.querySelector('#purchase-sound')
+const inputs = document.querySelectorAll('input')
 
 
 backArrow.addEventListener('click', () => {
     window.location = '../AddFundsPage/addfundspage.html'
 })
 
+balance.textContent = `$${user.balance}`
 
 
 // TASK: CREATE FUNCTION FOR FOCUSED BUTTONS
@@ -80,7 +87,57 @@ depositAmountField.addEventListener('change', () => {
 // TASK: CREATE FUNCTIONALITY FOR DEPOSIT BUTTON PRESS
 
 depositButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    const amountToBeDeposited = selectedButton ? Number(selectedButton.getAttribute('data-amount')) : Number(depositAmountField.value.slice(1)).toFixed(2);
-    console.log(amountToBeDeposited)
+    if (form.checkValidity()) {
+       e.preventDefault()
+        let amountToBeDeposited = selectedButton ? Number(selectedButton.getAttribute('data-amount')) : Number(depositAmountField.value.slice(1));
+        let amountToBeDepositedToFixed = Number(amountToBeDeposited.toFixed(2));
+        console.log(`The user wants: $${amountToBeDepositedToFixed} to be deposited to their account.`) 
+    
+        // TASK: CREATE FUNCTIONAITLY FOR ADDING DEPOSIT AMOUNT TO BALANCE
+        console.log(`${user.username}'s balance is:`, user.balance) 
+        // TASK: UPDATE LS VALUE TO NOW CURRENT VALUE
+        const userObject = JSON.parse(ls.getItem('currentUser'))
+        userObject.balance += amountToBeDepositedToFixed
+        console.log(userObject.balance)
+        // serialize the object so it can be store back in ls
+        const updatedUser = JSON.stringify(userObject)
+        ls.setItem('currentUser', updatedUser)
+        console.log(ls.getItem('currentUser'))
+
+        // TASK: UPDATE THE USER BALANCE INSIDE THE USER ARRAY
+        // identify the current user
+        const currentUserIdentifier = JSON.parse(ls.getItem('currentUser')).username
+
+        // get the users array
+        const usersString = ls.getItem('userArray')
+        // parse the user array
+        const usersStringToArray = JSON.parse(usersString)
+        console.log(usersStringToArray)
+        // find and update the user
+        const userIndex = usersStringToArray.findIndex(user => user.username === currentUserIdentifier )
+        if (userIndex !== -1) {
+            usersStringToArray[userIndex].balance += amountToBeDepositedToFixed
+            console.log(usersStringToArray[userIndex].balance)
+        }
+        // serialize the updated array
+        const updatedUserString = JSON.stringify(usersStringToArray)
+        console.log(updatedUserString)
+        // store the updated array
+        ls.setItem('userArray', updatedUserString)
+        console.log(user.balance)
+        balance.textContent = `$${userObject.balance}`
+        purchaseSound.play()
+        resetFields()
+    }
+    
+    
 });
+
+
+// TASK: CREATE FUNCTION TO RESET INPUT FIELDS AFTER DEPOSIT
+const resetFields = () => {
+    inputs.forEach((input) => {
+        input.value = ''
+    })
+    transactionText.textContent = 'Please enter a valid deposit amount.'
+}
